d82cfe2baf01e49e4a612553cdca8d99a669e0b6
Paul Kelley <Paul Kelley>
2017-05-09 13:46:06 -0600

    Creation of Redirects project

---
+++ Redirects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// A C# application to detect circular redirects.
+/// </summary>
+namespace Redirects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
