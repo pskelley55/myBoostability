ee0a6e520f3e00110464d03d1f021d45c2b3d507
Paul Kelley <Paul Kelley>
2017-05-09 14:18:07 -0600

    Create Interfaces and Models work directories

---
+++ Redirects/Interfaces/IRouteAnalyzer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Redirects.Interfaces
+{
+    public interface IRouteAnalyzer
+    {
+        IEnumerable<string> Process(IEnumerable<string> routes);
+    }
+}
---
+++ Redirects/Models/RouteModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Redirects.Interfaces;
+
+namespace Redirects.Models
+{
+    public class RouteModel : IRouteAnalyzer
+    {
+        public bool AddRoute(string route)
+        {
+            bool isGood = true;
+            return isGood;
+        }
+
+        #region IRouteAnalyzer Implementation
+        public IEnumerable<string> Process(IEnumerable<string> routes)
+        {
+            return null;
+        }
+        #endregion IRouteAnalyzer Implementation
+    }
+}
